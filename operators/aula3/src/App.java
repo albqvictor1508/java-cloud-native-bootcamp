c0ef6541be391307a9b80261aa773e57ed0f2631
albqvictor1508 <albq.victor@gmail.com>
2025-03-17 15:09:07 -0300

    aula3: operador de incremento e decremento

---
+++ operators/aula3/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      int number = 10;
+        System.out.println(number++); //vai imprimir primeiro pra depois incrementar
+        System.out.println(number--); //vai imprimir primeiro pra depois decrementar
+    }
+}
