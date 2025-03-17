1c4042bea6163ad185dcf6b2e79687934835ddb5
albqvictor1508 <albq.victor@gmail.com>
2025-03-17 15:34:17 -0300

    aula4: operadores ternários

---
+++ operators/aula4/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      boolean idk = true;
+      String result = idk == false ? "FALSO" : "VERDADEIRO";
+        System.out.println(result);
+    }
+}
