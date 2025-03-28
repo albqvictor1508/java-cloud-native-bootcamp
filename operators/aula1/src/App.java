135de22f54d6ef961446aee56baf97da4fb8d5e2
albqvictor1508 <albq.victor@gmail.com>
2025-03-28 10:55:12 -0300

    initial finalized

---
+++ operators/aula1/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      System.out.println("Ensinou sobre os operadores básicos e fez um exercício sobre concatenação");
+    }
+}
---
+++ operators/aula3/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      int number = 10;
+      boolean booleano = true;
+      booleano = !booleano;
+        System.out.println(number++); //vai imprimir primeiro pra depois incrementar
+        System.out.println(number--); //vai imprimir primeiro pra depois decrementar
+        System.out.println(booleano); //false (! = o contrário)
+    }
+}
