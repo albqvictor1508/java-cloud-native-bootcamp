ea9a9dd2920c8bc1d7a8dd8e2a971c7c2a696f51
albqvictor1508 <albq.victor@gmail.com>
2025-03-29 12:56:43 -0300

    add bank account constructor with args

--- bank-account-3/src/classes/BankAccount.java
+++ bank-account-3/src/classes/BankAccount.java
@@ -4,9 +4,14 @@ public class BankAccount {
     private String name;
     private double balance;
 
+    public BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
     public void checkBalance() {
         System.out.println("Hello, " + name + ", you have " + balance + " USD");
     }
 
-    
+
 }
