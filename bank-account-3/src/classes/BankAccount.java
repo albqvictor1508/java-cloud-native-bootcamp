d2752891f383b8c5a9e7cd3e133e16c8ee27b653
albqvictor1508 <albq.victor@gmail.com>
2025-03-29 12:54:43 -0300

    check balance method created

--- bank-account-3/src/classes/BankAccount.java
+++ bank-account-3/src/classes/BankAccount.java
@@ -1,5 +1,10 @@
 package classes;
 
 public class BankAccount {
+    private String name;
     private double balance;
+
+    public void checkBalance() {
+        System.out.println("Hello, " + name + ", you have " + balance + " USD");
+    }
 }
