97966f8da0537689ba67c68876fb33751d5a1a2c
albqvictor1508 <albq.victor@gmail.com>
2025-03-29 19:19:23 -0300

    car exercise finalized

--- car/src/App.java
+++ car/src/App.java
@@ -1,5 +1,8 @@
+import classes.Car;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Car savero = new Car();
+        savero.turnSide("direita");
     }
 }
