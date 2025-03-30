782f852c6ff9d6e43b67c2e3f36946ac8286cda1
albqvictor1508 <albq.victor@gmail.com>
2025-03-30 09:36:13 -0300

    bathing pet method completed

--- petshop/src/classes/Pet.java
+++ petshop/src/classes/Pet.java
@@ -3,5 +3,38 @@ package classes;
 public class Pet {
     private String name;
     private String breed;
-    private int age;  
+    private int age;
+    private boolean isDirty = true;  
+
+    public void setDirty(boolean isDirty) {
+        this.isDirty = isDirty;
+    }
+
+    public boolean getDirty() {
+        return isDirty;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
