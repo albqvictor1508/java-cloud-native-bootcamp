dbb4f705173f5de3d566fa24ee67866667d5745b
albqvictor1508 <albq.victor@gmail.com>
2025-03-30 16:33:58 -0300

    teacher constructor

--- abstract-classes/src/classes/Teacher.java
+++ abstract-classes/src/classes/Teacher.java
@@ -3,4 +3,10 @@ package classes;
 public non-sealed class Teacher extends Person {
     private String role;
     private double salary;
+
+    public Teacher(String name, String surname, int age, String role, int salary) {
+        super(name, surname, age);
+        this.role = role;
+        this.salary = salary;
+    }
 }
