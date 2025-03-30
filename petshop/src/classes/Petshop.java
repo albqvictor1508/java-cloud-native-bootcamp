96907543d5ca0260abe2a33153d2fb15e1159905
albqvictor1508 <albq.victor@gmail.com>
2025-03-30 09:44:27 -0300

    verify water, verify shampoo and remove pet methods created

--- petshop/src/classes/Petshop.java
+++ petshop/src/classes/Petshop.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Petshop {
     private List<Pet> pets;
-    private int water;
-    private int shampoo;
+    private int water = 30;
+    private int shampoo = 10;
 
     public void bathingPet() {
         if(pets.size() == 0) {
@@ -16,6 +16,8 @@ public class Petshop {
             System.out.println("The pet is already clean");
             return;
         }
+        this.shampoo -= 2;
+        this.water -= 10;
         pet.setDirty(false);
         System.out.println("The pet has been cleaned");
     }
@@ -25,4 +27,18 @@ public class Petshop {
         System.out.println(pet.getName() + ", of the breed " + pet.getBreed() + " has been added to the pets list");
     }
     
+    public void removePet() {
+        if(pets.size() == 0) {
+            System.out.println("The list of pets is empty");
+            return;
+        }
+        pets.remove(pets.size() - 1);
+    }
+
+    public void verifyWater() {
+        System.out.println(this.water + "L of water in machine");
+    }
+    public void verifyShampoo() {
+        System.out.println(this.shampoo + "L of shampoo in machine");
+    }
 }
