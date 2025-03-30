2132462fcb76decf99d8c01c475637911eb08c59
albqvictor1508 <albq.victor@gmail.com>
2025-03-30 16:30:38 -0300

    abstract class and more about sealed non-sealed and final

---
+++ abstract-classes/src/classes/Person.java
@@ -0,0 +1,13 @@
+package classes;
+
+public sealed class Person permits Teacher, Streamer {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+}
---
+++ abstract-classes/src/classes/Streamer.java
@@ -0,0 +1,19 @@
+package classes;
+
+public non-sealed class Streamer extends Person {
+    private String channel;
+    private int views;
+
+    public Streamer(String name, String surname, int age, String channel, int views) {
+        super(name, surname, age);
+        this.channel = channel;
+        this.views = views;
+    }
+    
+    public String getChannel() {
+        return channel;
+    }
+    public int getViews() {
+        return views;
+    }
+}
