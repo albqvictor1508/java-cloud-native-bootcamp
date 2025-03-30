003be7a3cc0a27f09d3e662c574a112704f9e296
albqvictor1508 <albq.victor@gmail.com>
2025-03-30 09:36:13 -0300

    bathing pet method completed

--- petshop/src/classes/Petshop.java
+++ petshop/src/classes/Petshop.java
@@ -4,5 +4,25 @@ import java.util.List;
 
 public class Petshop {
     private List<Pet> pets;
+    private int water;
+    private int shampoo;
+
+    public void bathingPet() {
+        if(pets.size() == 0) {
+            System.out.println("The list of pets is empty");
+        }
+        Pet pet = pets.get(0);
+        if(pet.getDirty() == true) {
+            System.out.println("The pet is already clean");
+            return;
+        }
+        pet.setDirty(false);
+        System.out.println("The pet has been cleaned");
+    }
+
+    public void addPet(Pet pet) {
+        pets.add(pet);
+        System.out.println(pet.getName() + ", of the breed " + pet.getBreed() + " has been added to the pets list");
+    }
     
 }
