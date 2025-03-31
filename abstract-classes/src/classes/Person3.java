db766d53fb3a6c085ef6c35ae97d40e48a6a78eb
albqvictor1508 <albq.victor@gmail.com>
2025-03-31 10:28:28 -0300

    abstract classes and more about inherance and polimophism

--- abstract-classes/src/classes/Person3.java
+++ abstract-classes/src/classes/Person3.java
@@ -1,11 +1,11 @@
 package classes;
 
-public sealed class Person permits Teacher, Streamer {
+public sealed class Person3 permits Teacher, Streamer {
     private String name;
     private String surname;
     private int age;
 
-    public Person(String name, String surname, int age) {
+    public Person3(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
