66db1ae0b00d0910cbd10ec1de0e82aef85ba0d1
albqvictor1508 <albq.victor@gmail.com>
2025-04-07 19:15:13 -0300

    books4 exercise completed

---
+++ collections/aula5/src/classes/Book.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class Book {
+    private String name;
+    private String author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name+ "- " + author;
+    }
+}
