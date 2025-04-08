ce1acd8e0437586f26bd9dee0ae10e7ebafba5ee
albqvictor1508 <albq.victor@gmail.com>
2025-04-08 10:10:50 -0300

    list interface finalized

--- collections/aula6/src/PeopleOrdenation.java
+++ collections/aula6/src/PeopleOrdenation.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class PeopleOrdenation {
@@ -16,5 +17,17 @@ public class PeopleOrdenation {
     public void sortByAge() {
         List<Person> peopleByAge = new ArrayList<>(people);
         Collections.sort(peopleByAge);
+    }
+
+    public void sortByHeight() {
+        List<Person> peopleByHeight = new ArrayList<>(people);
+        Collections.sort(peopleByHeight, new ComparatorByHeight());
+    }
+}
+
+class ComparatorByHeight implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2) {
+        return Double.compare(p1.height(), p2.height());
     }
 }
