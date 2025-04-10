c63d8d021297a0d053a358c85513d667c7c8272c
albqvictor1508 <albq.victor@gmail.com>
2025-04-10 08:41:20 -0300

    salve

--- collections/set/aula1/src/Inviteds.java
+++ collections/set/aula1/src/Inviteds.java
@@ -1,7 +1,12 @@
+import java.util.HashSet;
 import java.util.Set;
 
 public class Inviteds {
     private Set<Invited> inviteds;
+
+    public Inviteds() {
+        this.inviteds = new HashSet<>();
+    }
 
     public void addNewInvited(Invited invited) {
         this.inviteds.add(invited);
