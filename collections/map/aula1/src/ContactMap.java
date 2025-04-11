ac23e6059c2a933e7189875a0b43a5c162723e69
albqvictor1508 <albq.victor@gmail.com>
2025-04-10 23:21:23 -0300

    initializating map

--- collections/map/aula1/src/ContactMap.java
+++ collections/map/aula1/src/ContactMap.java
@@ -2,27 +2,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ContactMap {
-  private Map<String, String> contacts; //{name: phone} => n sei se a estrutura é realmente essa, mas a do JS é assim
+  private Map<String, Integer> contacts; //{name: phone} => n sei se a estrutura é realmente essa, mas a do JS é assim
 
   public ContactMap() {
     contacts = new HashMap<>();
   }
 
-  public void addNewContact(String name, String phone) {
+  public void addNewContact(String name, int phone) {
     contacts.put(name, phone);
   }
 
   public void removeContact(String name) {
-    if(contacts.containsKey(name)) {
+    if(!contacts.isEmpty()) {
       contacts.remove(name);
-    };
+    }
   }
 
   public void showContacts() {
     System.out.println(contacts);
   }
 
-  public void searchByName(String name) {
-    contacts.containsValue(name);
+  public Integer searchByName(String name) {
+    Integer numberByName = null;
+    if(contacts.containsKey(name)) {
+      numberByName = contacts.get(name);
+    }
+    return numberByName;
   }
 }
