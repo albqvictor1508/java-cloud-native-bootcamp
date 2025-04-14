fbbb5b869abdf5ca6421edb7feec3c036d05ec02
albqvictor1508 <albq.victor@gmail.com>
2025-04-14 10:32:18 -0300

    start game method completed

--- sudoku/src/App.java
+++ sudoku/src/App.java
@@ -1,3 +1,7 @@
+import static java.util.Objects.nonNull;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -10,7 +14,80 @@ public class App {
     public static void main(String[] args) throws Exception {
         final Map<String, String> positions = Stream.of(args).collect(Collectors.toMap(k -> k.split(";")[0], v -> v.split(";")[1]));
         while (true) {
+            System.out.println("Seleciona uma das opções a seguir:");
+            System.out.println("1 - Iniciar novo jogo.");
+            System.out.println("2 - Colocar um novo número");
+            System.out.println("3 - Remover número");
+            System.out.println("4 - Visualizar jogo atual");
+            System.out.println("5 - Verificar status do jogo");
+            System.out.println("6 - Limpar jogo");
+            System.out.println("7 - Finalizar jogo");
+            System.out.println("8 - Sair");
+
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1 -> startGame();
+                case 2 -> inputNumber();
+                case 3 -> removeNumber();
+                case 4 -> showCurrentGame();
+                case 5 -> showGameStatus();
+                case 6 -> clearGame();
+                case 7 -> finishGame();
             
+                case 8 -> System.exit(0);
+                default -> System.out.println("Opção inválida, selecione novamente.");
+            }
         }
     }
+    private static void startGame(final Map<String, String> positions) {
+        if(nonNull(positions)) {
+            System.out.println("O jogo já foi iniciado");
+            return;
+        }
+
+        List<List<Space>> spaces = new ArrayList<>();
+        for(int i = 0; i < BOARD_LIMIT; i++) {
+            spaces.add(new ArrayList<>());
+            for(int j = 0; j < BOARD_LIMIT; j++) {
+                String positionsConfig = positions.get("%s %s".formatted(i,j)); 
+                int expected = Integer.parseInt(positionsConfig.split(",")[0]);
+                boolean fixed = Boolean.parseBoolean(positionsConfig.split(",")[1]);
+                Space currentSpace = new Space(expected, fixed);
+                spaces.get(i).add(currentSpace);
+            }
+
+            board = new Board(spaces);
+            System.out.println("O jogo está pronto para começar!");
+        }
+        
+    }
+
+    private static Object inputNumber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'inputNumber'");
+    }
+
+    private static Object finishGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'finishGame'");
+    }
+    private static Object clearGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clearGame'");
+    }
+    private static Object showGameStatus() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showGameStatus'");
+    }
+    private static Object showCurrentGame() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showCurrentGame'");
+    }
+    private static Object removeNumber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeNumber'");
+    }
+
+
 }
