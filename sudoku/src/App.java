10601beb6f03bd865b1cade3e40935349f9bca20
albqvictor1508 <albq.victor@gmail.com>
2025-04-14 10:03:24 -0300

    creating sudoku

--- sudoku/src/App.java
+++ sudoku/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class App {
+    private final static Scanner scanner = new Scanner(System.in);
+    private static Board board;
+    private final static int BOARD_LIMIT = 9;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final Map<String, String> positions = Stream.of(args).collect(Collectors.toMap(k -> k.split(";")[0], v -> v.split(";")[1]));
+        while (true) {
+            
+        }
     }
 }
