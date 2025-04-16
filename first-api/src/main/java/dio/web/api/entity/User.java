d84a116e360c02bb7467bdb1a9beb5dcc5f37e53
albqvictor1508 <albq.victor@gmail.com>
2025-04-16 19:56:48 -0300

    fake repository created

--- first-api/src/main/java/dio/web/api/entity/User.java
+++ first-api/src/main/java/dio/web/api/entity/User.java
@@ -1,13 +1,15 @@
 package dio.web.api.entity;
 
 public class User {
+    private Long id;
     private String name;
     private String phone;
     private String password;
 
     public User() {}
 
-    public User(String name, String phone, String password) {
+    public User(Long id,String name, String phone, String password) {
+        this.id = id;
         this.name = name;
         this.phone = phone;
         this.password = password;
@@ -15,6 +17,10 @@ public class User {
 
     public String getName() {
         return name;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getPhone() {
