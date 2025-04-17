c0388286f8dcf98160c8951aa16a2b7fd06e2a57
albqvictor1508 <albq.victor@gmail.com>
2025-04-17 09:58:56 -0300

    web security config created

--- security/src/main/java/dio/web/security/config/WebSecurityConfig.java
+++ security/src/main/java/dio/web/security/config/WebSecurityConfig.java
@@ -1,14 +1,26 @@
 package dio.web.security.config;
 
+import dio.web.security.service.SecurityDatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    @Autowired
+    private SecurityDatabaseService service;
+
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(service).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
