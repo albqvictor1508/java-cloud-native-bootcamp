53318536176a33a21b4e880d8223b4f29d90946c
albqvictor1508 <albq.victor@gmail.com>
2025-04-17 14:53:21 -0300

    user save with password encoder created

--- security-jwt/src/main/java/dio/web/security_jwt/service/UserService.java
+++ security-jwt/src/main/java/dio/web/security_jwt/service/UserService.java
@@ -3,12 +3,20 @@ package dio.web.security_jwt.service;
 import dio.web.security_jwt.entity.User;
 import dio.web.security_jwt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository repository;
+    private PasswordEncoder passwordEncoder;
+
+    public void save(User user) {
+        String pass = user.getPassword();
+        user.setPassword(passwordEncoder.encode(pass));
+        repository.save(user);
+    }
 
     public User findByUsername(String username) {
         return repository.findByUsername(username);
