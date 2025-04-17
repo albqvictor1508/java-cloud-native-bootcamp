0ad154e27e62cb7e578e897aca60894d2552d408
albqvictor1508 <albq.victor@gmail.com>
2025-04-17 09:19:05 -0300

    configure adapter wip

--- security/src/main/java/config/WebSecurityConfig.java
+++ security/src/main/java/config/WebSecurityConfig.java
@@ -1,20 +1,21 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("security")
-                .password("{noop}1234")
-                .roles("USERS")
-                .and()
-                .withUser("admin")
-                .password("{noop}1649")
-                .roles("MANAGER");
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST,"/login").permitAll()
+                .antMatchers("/managers").hasAnyRole("MANAGERS")
+                .antMatchers("/users").hasAnyRole("USERS","MANAGERS")
+                .anyRequest().authenticated().and().httpBasic();
     }
 }
