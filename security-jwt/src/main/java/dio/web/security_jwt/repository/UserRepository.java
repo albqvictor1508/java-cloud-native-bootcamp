32c800cfb7f78e5b84c277d0a32007bf918489e8
albqvictor1508 <albq.victor@gmail.com>
2025-04-17 12:01:47 -0300

    user repository created

--- security-jwt/src/main/java/dio/web/security_jwt/repository/UserRepository.java
+++ security-jwt/src/main/java/dio/web/security_jwt/repository/UserRepository.java
@@ -1,1 +1,10 @@
-s
+package dio.web.security_jwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dio.web.security_jwt.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    
+}
