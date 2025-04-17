3b25c5ab1c326a26c41a7b10c7dd44b67831258e
albqvictor1508 <albq.victor@gmail.com>
2025-04-17 08:50:00 -0300

    hello endpoint created

--- security/src/main/java/controller/WelcomeController.java
+++ security/src/main/java/controller/WelcomeController.java
@@ -1,4 +1,16 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
 public class WelcomeController {
+
+    @GetMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello, %s!".formatted(name);
+    }
 }
