d1825b8654341c5256230e16ab3675fc1489ab62
albqvictor1508 <albq.victor@gmail.com>
2025-04-18 10:43:17 -0300

    singleton tested

--- dio.web.padroes-projeto/src/Main.java
+++ dio.web.padroes-projeto/src/Main.java
@@ -1,7 +1,24 @@
+import singleton.SingletonEager;
+import singleton.SingletonLazy;
+import singleton.SingletonLazyHolder;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("bom dia krl");
+        SingletonLazy lazy = SingletonLazy.getInstance("victor",18);
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstance("victor",18);
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
     }
 }
