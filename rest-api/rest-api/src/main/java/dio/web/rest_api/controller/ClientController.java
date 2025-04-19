8bbae29f4828e199f687377e4742c3307b201f8d
albqvictor1508 <albq.victor@gmail.com>
2025-04-18 23:57:09 -0300

    service initialized

---
+++ rest-api/rest-api/src/main/java/dio/web/rest_api/controller/ClientController.java
@@ -0,0 +1,13 @@
+package dio.web.rest_api.controller;
+
+import dio.web.rest_api.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+    @Autowired
+    private ClientService service;
+}
--- rest-api/rest-api/src/main/java/dio/web/rest_api/service/ViaCepService.java
+++ rest-api/rest-api/src/main/java/dio/web/rest_api/service/ViaCepService.java
@@ -2,12 +2,12 @@ package dio.web.rest_api.service;
 
 import dio.web.rest_api.model.Address;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "viacep", url = "viacep.com.br/ws")
 public interface ViaCepService {
-    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    @GetMapping("/{cep}/json/")
     Address consultCep(@PathVariable("cep") String cep);
 }
