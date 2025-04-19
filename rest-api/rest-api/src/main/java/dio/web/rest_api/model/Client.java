45bd632658329fb02147ff4a356a548bd8e19028
albqvictor1508 <albq.victor@gmail.com>
2025-04-18 23:39:26 -0300

    user model finished

---
+++ rest-api/rest-api/src/main/java/dio/web/rest_api/model/Client.java
@@ -0,0 +1,23 @@
+package dio.web.rest_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cep;
+}
