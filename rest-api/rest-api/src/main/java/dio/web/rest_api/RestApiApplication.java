9c1d9b1a46261ee33c9d6d8e317dd43a1eddfba4
albqvictor1508 <albq.victor@gmail.com>
2025-04-18 21:27:07 -0300

    rest api wip

--- rest-api/rest-api/src/main/java/dio/web/rest_api/RestApiApplication.java
+++ rest-api/rest-api/src/main/java/dio/web/rest_api/RestApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class RestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
