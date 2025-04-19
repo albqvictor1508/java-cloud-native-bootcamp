ac93b1d0e2249a2e5b27a502ba9935bf0498dab2
albqvictor1508 <albq.victor@gmail.com>
2025-04-19 11:32:24 -0300

    client features incoming

--- rest-api/rest-api/src/main/java/dio/web/rest_api/controller/ClientController.java
+++ rest-api/rest-api/src/main/java/dio/web/rest_api/controller/ClientController.java
@@ -1,7 +1,10 @@
 package dio.web.rest_api.controller;
 
+import dio.web.rest_api.model.Client;
 import dio.web.rest_api.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClientController {
     @Autowired
     private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Client>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
 }
--- rest-api/rest-api/src/main/java/dio/web/rest_api/service/ClientService.java
+++ rest-api/rest-api/src/main/java/dio/web/rest_api/service/ClientService.java
@@ -1,15 +1,26 @@
 package dio.web.rest_api.service;
 
+import dio.web.rest_api.model.Client;
 import dio.web.rest_api.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    public void findById(Long id) {
+    public Client findById(Long id) {
+        if(!(repository.existsById(id))) {
+            return null;
+        }
+        Client client = repository.findById(id).get();
+        return client;
+    }
 
+    public Iterable<Client> findAll() {
+        return repository.findAll();
     }
 }
