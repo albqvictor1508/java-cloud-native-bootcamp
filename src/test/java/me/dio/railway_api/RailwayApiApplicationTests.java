19cef0aa5418e83f0dc3ef86d1a2770e8d414049
albqvictor1508 <albq.victor@gmail.com>
2025-04-20 11:42:45 -0300

    initial commit

---
+++ src/test/java/me/dio/railway_api/RailwayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.railway_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
