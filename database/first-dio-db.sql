e0d805f299cd1fa7bfe759a98b17af76044e1c28
albqvictor1508 <albq.victor@gmail.com>
2025-04-22 07:51:52 -0300

    table 'reserves' parcially created

--- database/first-dio-db.sql
+++ database/first-dio-db.sql
@@ -11,3 +11,13 @@ CREATE TABLE destinies (
   name VARCHAR(100) NOT NULL,
   description VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE reserves (
+	id INT,
+  user_id INT,
+  destiny_id INT,
+  reserve_date DATE NOT NULL,
+  status VARCHAR(255) DEFAULT 'pending' COMMENT 'Reserve status (confirmed, pending, canceled)'
+);
+
+
