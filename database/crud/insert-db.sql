c0023324dbe49d1360a5e6cd01a5a187004fb7e5
albqvictor1508 <albq.victor@gmail.com>
2025-04-22 09:47:47 -0300

    crud sql basics

---
+++ database/crud/insert-db.sql
@@ -0,0 +1,8 @@
+INSERT INTO users VALUES(
+	1,"victor", "test@test.com", "Conjunto", "2006-11-08" 
+)
+INSERT INTO destinies (id, name, description) VALUES (1,"Canadá", "quero muito ir pro Canadá")
+
+INSERT INTO reserves (id, user_id, destiny_id, reserve_date, status) VALUES (
+    1,1,1,"2025-07-11", "confirmed"
+)
