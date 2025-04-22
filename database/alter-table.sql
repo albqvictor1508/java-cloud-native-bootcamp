2d8ecd8a42e7062df59428dec7025b1399efef4c
albqvictor1508 <albq.victor@gmail.com>
2025-04-22 10:26:11 -0300

    constraints

--- database/alter-table.sql
+++ database/alter-table.sql
@@ -1,3 +1,14 @@
 ALTER TABLE users RENAME userss;
 
 ALTER TABLE users MODIFY COLUMN address VARCHAR(150);
+
+-- setando o user_id para auto increment 
+ALTER TABLE reserves
+MODIFY COLUMN user_id INT AUTO_INCREMENT,
+ADD CONSTRAINT fk_reserves_users
+FOREIGN KEY(user_id) REFERENCES users(id); 
+
+ALTER TABLE reserves
+MODIFY COLUMN destiny_id INT AUTO_INCREMENT,
+ADD CONSTRAINT fk_reserves_destinies
+FOREIGN KEY(destiny_id) REFERENCES destinies(id);
