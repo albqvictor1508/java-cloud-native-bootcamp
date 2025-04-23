e8c603e8593cd5fbbbd854b3ff8a802fdbaa78bd
albqvictor1508 <albq.victor@gmail.com>
2025-04-23 11:28:17 -0300

    more about join, now using left join

--- database/joins/join-tables-on-queries.sql
+++ database/joins/join-tables-on-queries.sql
@@ -12,3 +12,8 @@ WHERE u.id = 2;
 SELECT * FROM users u
 LEFT JOIN  reserves r ON r.user_id = u.id
 WHERE u.id = 1;
+
+SELECT r.id, r.user_id, r.destiny_id, r.reserve_date, u.name, d.name ,d.description FROM reserves r
+LEFT JOIN users u ON r.user_id = u.id
+LEFT JOIN destinies d ON r.destiny_id = d.id
+ORDER BY destiny_id ASC;
