7eb4cad5744f052672140dd82b9503ad7277e62d
albqvictor1508 <albq.victor@gmail.com>
2025-04-23 10:51:01 -0300

    inner join 2

--- database/joins/join-tables-on-queries.sql
+++ database/joins/join-tables-on-queries.sql
@@ -3,3 +3,8 @@ SELECT * FROM users u INNER JOIN reserves r ON r.user_id = u.id;
 SELECT * FROM users u 
 INNER JOIN reserves r ON r.user_id = u.id
 INNER JOIN destinies d ON d.id = r.destiny_id;
+
+SELECT * FROM reserves r 
+INNER JOIN users u ON r.user_id = u.id
+INNER JOIN destinies d ON r.destiny_id = d.id
+WHERE u.id = 2;
