82caf3315cfe8e7d196e813cf7fda2786363083c
albqvictor1508 <albq.victor@gmail.com>
2025-04-24 15:58:15 -0300

    employee entity created by the cavern men mode

---
+++ src/main/java/me/dio/jdbc/persistence/EmployeeDAO.java
@@ -0,0 +1,4 @@
+package me.dio.jdbc.persistence;
+
+public class EmployeeDAO {
+}
---
+++ src/main/java/me/dio/jdbc/persistence/entity/Employee.java
@@ -0,0 +1,14 @@
+package me.dio.jdbc.persistence.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Data
+public class Employee {
+    private long id;
+    private BigDecimal salary;
+    private String name;
+    private OffsetDateTime birthday;
+}
