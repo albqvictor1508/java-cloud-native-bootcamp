4f8b78521b5af7ba9d570cea9586e47d3c0c80e2
albqvictor1508 <albq.victor@gmail.com>
2025-04-24 16:04:38 -0300

    employee dao

--- src/main/java/me/dio/jdbc/JdbcApplication.java
+++ src/main/java/me/dio/jdbc/JdbcApplication.java
@@ -1,10 +1,12 @@
 package me.dio.jdbc;
 
+import me.dio.jdbc.persistence.EmployeeDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JdbcApplication {
+	private EmployeeDAO employeeDAO = new EmployeeDAO();
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcApplication.class, args);
