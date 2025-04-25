22a6626eec5511366b1b4fbeb8f6f222fac077e8
albqvictor1508 <albq.victor@gmail.com>
2025-04-25 12:33:02 -0300

    add final to employee dao params

--- src/main/java/me/dio/jdbc/persistence/EmployeeDAO.java
+++ src/main/java/me/dio/jdbc/persistence/EmployeeDAO.java
@@ -4,18 +4,18 @@ import me.dio.jdbc.persistence.entity.Employee;
 
 public class EmployeeDAO {
 
-    public void insert(Employee e) {
+    public void insert(final Employee e) {
 
     }
 
-    public Employee findById(Long id) {
+    public Employee findById(final Long id) {
         return null;
     }
 
-    public Employee update(Employee e) {
+    public Employee update(final Employee e) {
         return null;
     }
 
-    public void delete(Long id) {
+    public void delete(final Long id) {
     }
 }
